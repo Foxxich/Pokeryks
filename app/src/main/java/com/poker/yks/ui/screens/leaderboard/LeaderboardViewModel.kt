a1a0c34ed05d746f242fb89d035b2902d49c3428
Foxxich <64288356+Foxxich@users.noreply.github.com>
2023-12-18 20:46:09 +0100

    Implemented the leaderboard side of app

--- app/src/main/java/com/poker/yks/ui/screens/leaderboard/LeaderboardViewModel.kt
+++ app/src/main/java/com/poker/yks/ui/screens/leaderboard/LeaderboardViewModel.kt
@@ -1,6 +1,5 @@
 package com.poker.yks.ui.screens.leaderboard
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.poker.yks.data.LeaderboardUser
@@ -19,22 +18,19 @@ class LeaderboardViewModel : ViewModel() {
     private val _players = MutableStateFlow<List<LeaderboardUser>>(emptyList())
     val players = _players.asStateFlow()
 
+    init {
+        getLeadearboard() // Inicjalizacja danych przy tworzeniu ViewModel
+    }
+
     fun getLeadearboard() {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
-
-                Log.i("herrrrre", "0")
                 val remote = repository.getLeaderboardList()
-                Log.i("herrrrre", "1")
                 if (remote.isSuccessful) {
-                    Log.i("herrrrre", "12")
                     val data = remote.body()
-                    Log.i("herrrrre", "13")
                     if (data != null) {
                         _players.update { data }
                     }
-
-
                 }
             }
         }
--- app/src/main/java/com/poker/yks/ui/screens/registration/RegistrationScreen.kt
+++ app/src/main/java/com/poker/yks/ui/screens/registration/RegistrationScreen.kt
@@ -8,7 +8,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
